bbb6d13c090bfcff9e8472f8098638da81fffe48
mp-38 <maksim.pisaryk@louisville.edu>
2019-03-19 08:42:54 -0400

    login, protected routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://mmpisa02:8Djctvm*@cluster0-n1y2w.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://mmpisa02:8Djctvm*@cluster0-n1y2w.mongodb.net/test?retryWrites=true',
+    secretOrKey: 'secret'
 };
